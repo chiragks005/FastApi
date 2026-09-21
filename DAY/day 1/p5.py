a44174e7b8c387afc2ab0d4d4c679e1912e3c0c0
chiragks005 <2025cs_chiragks_b@nie.ac.in>
2026-09-21 14:49:03 +0530

    Decorator

---
+++ DAY/day 1/p5.py
@@ -0,0 +1,11 @@
+# Concept: decorator is a python feature that lets  you to modify a function using @symbol
+def my_decorator(func):
+    def wrapper():
+        print("Before")
+        func()
+        print("After")
+    return wrapper
+@my_decorator
+def say_hello():
+    print("Hello !")
+say_hello()
