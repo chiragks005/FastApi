18ec62d5d2041a3ca86d98805d34029b27535cfa
chiragks005 <2025cs_chiragks_b@nie.ac.in>
2026-09-22 15:21:57 +0530

    all basics of fastapi

--- DAY/day 2/Fastapipractice/main.py
+++ DAY/day 2/Fastapipractice/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
 @app.get("/")
 def home():
@@ -9,3 +10,25 @@ def about():
 @app.get("/health")
 def health():
     return {"Status":"ok"}
+#postreq
+@app.post("/create")
+def create_some():
+    return {"messege":"Created"}
+
+
+#path
+@app.get("/student/{usn}")
+def get_result(usn):
+    return {"Result ":"Distinction","USN":usn}
+
+@app.get("/candidate/{rollno}")
+def get_date(rollno:int):
+    return {"Result ":"Distinction","roll":rollno,"type":str(type(rollno))}
+
+class Item(BaseModel):
+    name : str
+    price : float
+    in_stock : bool = True
+@app.post("/items")   
+def create_item(item : Item):
+    return {"received":item,"total_price":item.price*1.18}
