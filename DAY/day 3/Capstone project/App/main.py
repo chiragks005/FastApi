6e07be0bb6c480fa76634732649a5ff7e290b1c5
chiragks005 <2025cs_chiragks_b@nie.ac.in>
2026-09-25 09:32:08 +0530

    day 3 done

--- DAY/day 3/Capstone project/App/main.py
+++ DAY/day 3/Capstone project/App/main.py
@@ -8,6 +8,9 @@ from App.routers import categories
 from App.routers import tickets
 from App.routers import comments
 from App.routers import attachments
+from App.routers import audit_logs
+
+
 
 # Creating FastAPI app instance
 app = FastAPI(title=settings.APP_NAME)
@@ -17,7 +20,7 @@ app.include_router(categories.router)
 app.include_router(tickets.router)
 app.include_router(comments.router)
 app.include_router(attachments.router)
-
+app.include_router(audit_logs.router)
 # This function runs once when the server starts. It checks the DB connection.
 @app.on_event("startup")
 def on_startup() -> None:
